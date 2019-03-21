66366dd989516dc38fb5f2e23f511f08d7bd6197
Hiroaki Nakamura <hnakamur@gmail.com>
2019-03-21 17:17:45 +0900

    Modify manifest for Firefox, changing options_page to options_ui

--- src/js/options.js
+++ src/js/options.js
@@ -1,1 +1,13 @@
 import "../css/options.css";
+import App from '../svelte/Options.svelte';
+
+const app = new App({
+	target: document.body,
+	data: {
+		name: 'options'
+	}
+});
+
+window.app = app;
+
+export default app;
